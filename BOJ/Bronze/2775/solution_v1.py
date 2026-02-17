45f18907beb72b9ecbb13f7dd7d35606bac333b7
wonzunyup <wjy1126@dgu.ac.kr>
2026-02-18 01:08:20 +0900

    Update solution_v1.py

--- BOJ/Bronze/2775/solution_v1.py
+++ BOJ/Bronze/2775/solution_v1.py
@@ -11,7 +11,7 @@ for _ in range(T):
     for i in range(n + 1):
         dp[0][i] = i
     for i in range(k + 1):
-        dp[k][1] = 1
+        dp[i][1] = 1
     for i in range(1, k + 1):
         for j in range(1, n + 1):
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
