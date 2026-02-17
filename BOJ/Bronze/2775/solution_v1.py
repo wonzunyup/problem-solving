44e33de05e099d42c5fcaf06ca7e20d9adbf068f
wonzunyup <wjy1126@dgu.ac.kr>
2026-02-18 01:05:51 +0900

    Create solution_v1.py

---
+++ BOJ/Bronze/2775/solution_v1.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+out = []
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    dp = [[0] * (n + 1)] * (k + 1)
+    for i in range(n + 1):
+        dp[0][i] = i
+    for i in range(k + 1):
+        dp[k][1] = 1
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    out.append(str(dp[k][n]) + "\n")
+
+sys.stdout.write("".join(out))
