b84bf9db42e7046dffbf773f9b6cde2915868257
wonzunyup <wjy1126@dgu.ac.kr>
2026-02-28 21:35:33 +0900

    Create solution_v2.py

---
+++ BOJ/Bronze/2775/solution_v2.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
+
+    for i in range(n + 1):
+        dp[0][i] = i
+
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    print(dp[i][j])
