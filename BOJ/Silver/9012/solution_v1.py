6a786f023554c907ddb1e4f55b4d34f5d28021f6
wonzunyup <wjy1126@dgu.ac.kr>
2026-03-15 20:45:03 +0900

    Update solution_v1.py

--- BOJ/Silver/9012/solution_v1.py
+++ BOJ/Silver/9012/solution_v1.py
@@ -3,17 +3,19 @@ input = sys.stdin.readline
 
 T = int(input())
 out = []
+
 for _ in range(T):
     string = input().rstrip()
-    
     stack = []
+
     for ch in string:
         if ch == "(":
             stack.append(ch)
-        else :
+        else:
             if not stack:
                 out.append("NO")
                 break
+            stack.pop()
     else:
         out.append("YES" if not stack else "NO")
 
