ea7752e82c84067252a3d8d235aade6325cf0392
wonzunyup <wjy1126@dgu.ac.kr>
2026-03-28 20:25:20 +0900

    Create solution_v2.py

---
+++ BOJ/Silver/11651/solution_v2.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+spots = [tuple(map(int, input().split())) for _ in range(N)]
+spots.sort(key=lambda x: (x[1], x[0]))
+
+for x, y in spots:
+    print(x, y)
