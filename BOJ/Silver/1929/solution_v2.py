f698e3321a1778ace8df2904a830f0c27fde3d3c
wonzunyup <wjy1126@dgu.ac.kr>
2026-03-30 23:20:29 +0900

    Create solution_v2.py

---
+++ BOJ/Silver/1929/solution_v2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+is_prime = [True] * (N + 1)
+if N >= 0 :
+    is_prime[0] = False
+if N >= 1:
+    is_prime[1] = False
+
+for i in range(2, int(N ** 0.5) + 1) :
+    if is_prime[i] :
+        for j in range(i * i, N + 1, i) :
+            is_prime[j] = False
+
+primes = [i for i in range(M, N + 1) if is_prime[i]]
+sys.stdout.write("\n".join(map(str, primes)))
