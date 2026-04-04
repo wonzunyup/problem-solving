8575be6844fe8ada0300bc32d026d5b0ccc73e26
wonzunyup <wjy1126@dgu.ac.kr>
2026-04-04 23:48:50 +0900

    Create solution_v2.py

---
+++ BOJ/Silver/17219/solution_v2.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+passwords = {}
+for _ in range(N):
+    address, password = input().split()
+    passwords[address] = password
+
+out = []
+for _ in range(M):
+    out.append(passwords[input().rstrip()])
+
+sys.stdout.write("\n".join(out))
